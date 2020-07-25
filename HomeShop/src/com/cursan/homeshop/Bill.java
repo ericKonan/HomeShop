c352c7294d21ebafa69f58db39d213a73c29ae82
ericKonan <erickonan.mail@gmail.com>
2020-07-25 10:45:12 +0000

    Update with interface

--- HomeShop/src/com/cursan/homeshop/Bill.java
+++ HomeShop/src/com/cursan/homeshop/Bill.java
@@ -1,22 +1,24 @@
 package com.cursan.homeshop;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Bill {
-    private Customer customer;
-    private Map<Product, Integer> products;
+    private Client client;
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
+    private Delivery delivery;
 
-    /**
-     * Add a product with a quantity in the bill
-     * @param product product to add
-     * @param quantity quantity of the product
-     */
-    public void addProduct(Product product, Integer quantity) {
+    public Bill(Client client, Delivery delivery) {
+        this.client = client;
+        this.delivery = delivery;
+    }
+
+    public void addProduct(Product product, int quantity) {
         this.products.put(product, quantity);
     }
 
-    public Customer getCustomer() {
-        return customer;
+    public Client getClient() {
+        return client;
     }
 
     public Map<Product, Integer> getProducts() {
